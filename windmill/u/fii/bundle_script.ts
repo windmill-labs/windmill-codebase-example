d05e2730ec8f45ac8659960f1f39c1b3edfe92cc
Ruben Fiszel <ruben@rubenfiszel.com>
2025-01-26 09:50:12 +0100

    update metadata

--- windmill/u/fii/bundle_script.ts
+++ windmill/u/fii/bundle_script.ts
@@ -1,7 +1,7 @@
 import { test } from "../../../src/index.ts";
 import * as cowsay from "cowsay";
 
-export async function main(x: string) {
+export async function main(x: string, y: number) {
   console.log(cowsay.say({ text: "XXXXXX" }));
   test(x);
   console.log("BAR");
